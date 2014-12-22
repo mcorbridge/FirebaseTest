fdbb08cee63b9f42c3911d8575fb2d3d3b68cdbf
michael corbridge <mcorbridge@gmail.com>
2014-12-22 08:23:29 -0500

    unit test

--- app/src/androidTest/java/com/mcorbridge/firebasetest/ApplicationTest.java
+++ app/src/androidTest/java/com/mcorbridge/firebasetest/ApplicationTest.java
@@ -1,13 +1,28 @@
 package com.mcorbridge.firebasetest;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ImageView;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+
+
+   public ApplicationTest(){
+       super(MainActivity.class);
+   }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = getActivity();
+    }
+
+    public void testImageViewNotNull(){
+        ImageView imageView = (ImageView)mainActivity.findViewById(R.id.imageView);
+        assertNotNull(imageView);
     }
 }
